64b7d085496304cffb19c04574160ad332c0cdd9
Nayan <ndhabarde11@gmail.com>
2020-06-28 18:08:24 +0530

    Added git ignore

---
+++ problem_1.py
@@ -0,0 +1,40 @@
+def sqrt(number):
+    """
+    Calculate the floored square root of a number
+
+    Args:
+       number(int): Number to find the floored squared root
+    Returns:
+       int: Floored Square Root
+    """
+    if number == 0:
+        return 0
+
+    if number == 1:
+        return 1
+    return find_sqrt(number, 0, number)
+
+    pass
+
+def find_sqrt(number, start, end):
+
+    if start == end:
+        return start - 1
+
+    mid = (start + end) // 2
+
+    square = mid * mid
+
+    if square == number:
+        return mid
+    elif square < number:
+        return find_sqrt(number, mid + 1, end)
+    else:
+        return find_sqrt(number, start, mid)
+
+
+print ("Pass" if  (3 == sqrt(9)) else "Fail")
+print ("Pass" if  (0 == sqrt(0)) else "Fail")
+print ("Pass" if  (4 == sqrt(16)) else "Fail")
+print ("Pass" if  (1 == sqrt(1)) else "Fail")
+print ("Pass" if  (5 == sqrt(27)) else "Fail")
